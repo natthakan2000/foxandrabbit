f60d345bf40af21047988bc87eebe245947cb711
Natthakan Euaumpon <natthakaneuaumpon@gmail.com>
2020-07-25 05:10:58 +0700

    changes

---
+++ src/main/java/io/muic/ooc/fab/Factory/Animal.java
@@ -0,0 +1,126 @@
+package io.muic.ooc.fab.Factory;
+
+import io.muic.ooc.fab.Field;
+import io.muic.ooc.fab.Location;
+
+import java.util.Random;
+
+public abstract class Animal implements Methods {
+    // Characteristics shared by all animals (class variables).
+
+    protected static final Random RANDOM = new Random();
+
+    private boolean alive = true;
+
+    private Location location;
+
+    protected Field field;
+
+    private int age = 0;
+
+    public abstract Location moveToNewLocation();
+
+    protected abstract int getBreedingAge();
+
+    protected abstract int getMaxLiterSize();
+
+    protected abstract double getBreedingProbability();
+
+    protected abstract int getMaxAge();
+
+    @Override
+    public void init(boolean randomAge, Field field, Location location) {
+        this.field = field;
+        setLocation(location);
+        if (randomAge) {
+            age = RANDOM.nextInt(getMaxAge());
+        }
+    }
+
+    @Override
+    public void act(List<Methods> newAnimals) {
+        incrementAge();
+        if (isAlive()) {
+            giveBirth(newAnimals);
+
+            Location newLocation = moveToNewLocation();
+
+            if (newLocation != null) {
+                setLocation(newLocation);
+            } else {
+                setDead();
+            }
+        }
+    }
+
+    @Override
+    public boolean isAlive() {
+        return alive;
+    }
+
+    @Override
+    public void setDead() {
+        alive = false;
+        if (location != null) {
+            field.clear(location);
+            location = null;
+            field = null;
+        }
+    }
+
+    @Override
+    public Location getLocation() {
+        return location;
+    }
+
+
+    @Override
+    public void setLocation(Location newLocation) {
+        if (location != null) {
+            field.clear(location);
+        }
+        location = newLocation;
+        field.place(this, newLocation);
+    }
+
+    @Override
+    public void incrementAge() {
+        age++;
+        if (age > getMaxAge()) {
+            setDead();
+        }
+    }
+
+    @Override
+    public void giveBirth(List<Methods> newAnimals) {
+
+        List<Location> free = field.getFreeAdjacentLocations(location);
+        int births = breed();
+        for (int b = 0; b < births && free.size() > 0; b++) {
+            Location loc = free.remove(0);
+
+            Animal young = breedOne(field, loc);
+            newAnimals.add(young);
+        }
+    }
+
+    @Override
+    public Animal breedOne(Field field, Location location) {
+        return AnimalFactory.createAnimal(getClass(), field, location);
+    }
+
+
+    @Override
+    public int breed() {
+        int births = 0;
+        if (canBreed() && RANDOM.nextDouble() <= getBreedingProbability()) {
+            births = RANDOM.nextInt(getMaxLiterSize()) + 1;
+        }
+        return births;
+    }
+
+    @Override
+    public boolean canBreed() {
+        return age >= getBreedingAge();
+    }'
+}
---
+++ src/main/java/io/muic/ooc/fab/Factory/Methods.java
@@ -0,0 +1,77 @@
+package io.muic.ooc.fab.Factory;
+
+import io.muic.ooc.fab.Field;
+import io.muic.ooc.fab.Location;
+
+import java.util.List;
+
+public interface Methods {
+    /**
+     * Create a new animal. An animal may be created with age zero (a new born)
+     * or with a random age.
+     *
+     * @param randomAge If true, the animal will have a random age.
+     * @param field     The field currently occupied.
+     * @param location  The location within the field.
+     */
+    void init(boolean randomAge, Field field, Location location);
+
+
+    void act(List<Methods> newAnimals);
+
+    /**
+     * Check whether the animal is alive or not.
+     *
+     * @return true if the animal is still alive.
+     */
+    boolean isAlive();
+
+    /**
+     * Indicate that the animal is no longer alive. It is removed from the
+     * field.
+     */
+    void setDead();
+
+    /**
+     * Return the animal's location.
+     *
+     * @return The animal's location.
+     */
+    Location getLocation();
+
+    /**
+     * Place the animal at the new location in the given field.
+     *
+     * @param newLocation The animal's new location.
+     */
+    void setLocation(Location newLocation);
+
+    /**
+     * Increase the age. This could result in the animal's death.
+     */
+    void incrementAge();
+
+    /**
+     * Check whether or not this animal is to give birth at this step. New
+     * births will be made into free adjacent locations.
+     *
+     * @param newAnimals A list to return newly born animals.
+     */
+    void giveBirth(List<Methods> newAnimals);
+
+    Animal breedOne(Field field, Location location);
+
+    /**
+     * Generate a number representing the number of births, if it can breed.
+     *
+     * @return The number of births (may be zero).
+     */
+    int breed();
+
+    /**
+     * A rabbit can breed if it has reached the breeding age.
+     *
+     * @return true if the rabbit can breed, false otherwise.
+     */
+    boolean canBreed();
+}
